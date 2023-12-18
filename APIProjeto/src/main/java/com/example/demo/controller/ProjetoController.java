7ba92699a1afdc65afabe38883d43d8dbe234c37
Vitoria Silva <106354254+Underzika@users.noreply.github.com>
2023-12-18 17:18:11 -0300

    Update ProjetoController.java
    crossorigin arrumado

--- APIProjeto/src/main/java/com/example/demo/controller/ProjetoController.java
+++ APIProjeto/src/main/java/com/example/demo/controller/ProjetoController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.entities.Projeto;
 import com.example.demo.repository.ProjetoRepository;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/Projetos")
 public class ProjetoController {
